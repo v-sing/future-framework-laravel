b01ba0afc06a59df84fd22a6b86bb36956a87dae
v-sing <email1946367301@163.com>
2019-06-03 18:17:01 +0800

    xxx

---
+++ app/Model/LogsSqls.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 紫云沫雪こ
+ * Email:email1946367301@163.com
+ * Date: 2019/5/27 0027
+ * Time: 17:29
+ */
+
+namespace App\Model;
+
+
+use Future\Admin\Auth\Database\BaseModel;
+
+class LogsSqls extends BaseModel
+{
+    protected $fillable = ['*'];
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $connection = config('admin.database.connection') ?: config('database.default');
+        $this->setConnection($connection);
+        $class = class_basename(get_class());
+        $this->setTable(parse_underline($class));
+        parent::__construct($attributes);
+    }
+}
